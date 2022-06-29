b824e9980bd68035a40e0b80dd300f56d12f37ba
MelihOzaslan01 <MelihOzaslan01@gmail.com>
2022-06-30 00:47:17 +0300

    IOC container added for Infrastructure

---
+++ Shopping.Infrastructure/DependencyContainer.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Shopping.Application.Common.Interfaces.Repositories;
+using Shopping.Infrastructure.Persistence;
+using Shopping.Infrastructure.Persistence.Repositories;
+
+namespace Shopping.Infrastructure
+{
+    public static class DependencyContainer
+    {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(configuration.GetConnectionString("Default"))
+            );
+
+            services.AddTransient<IProductRepository, ProductRepository>();
+            services.AddTransient<IShoppingListRepository, ShoppingListRepository>();
+            services.AddTransient<IUserRepository, UserRepository>();
+
+            return services;
+        }
+    }
+}
