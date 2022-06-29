338645915232c5e291c688f0a9ef3bbaa3c5204c
MelihOzaslan01 <MelihOzaslan01@gmail.com>
2022-06-30 00:47:53 +0300

    Product Dto added

---
+++ Shopping.Domain/Dtos/ProductDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Shopping.Domain.Dtos
+{
+    public class ProductDto
+    {
+        public int ShoppingListId { get; set; }
+        public string Name { get; set; }
+        public string Amount { get; set; }
+    }
+}
