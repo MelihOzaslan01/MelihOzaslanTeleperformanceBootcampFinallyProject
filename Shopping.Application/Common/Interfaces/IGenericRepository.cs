3adbd332e42eb0fb950e391947be7460a2a2526d
MelihOzaslan01 <MelihOzaslan01@gmail.com>
2022-06-30 00:45:25 +0300

    Application layer created and generic repository added

---
+++ Shopping.Application/Common/Interfaces/IGenericRepository.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using Shopping.Domain.Entities;
+
+namespace Shopping.Application.Common.Interfaces
+{
+
+    public interface IGenericRepository<T>: IGenericCommandRepository<T>, 
+        IGenericQueryRepository<T> where T : class,IBaseEntity,new()
+    {
+
+    }
+    public interface IGenericCommandRepository<T> where T : class
+    {
+        Task<bool> Add(T entity);
+        Task<bool> Update(T entity);
+        Task<bool> Delete(T entity);
+    }
+
+    public interface IGenericQueryRepository<T> where T : class
+    {
+        Task<IEnumerable<T>> GetAll();
+        Task<T> GetById(int id);
+        Task<IEnumerable<T>> Get(Expression<Func<T,bool>> expression);
+
+    }
+}
---
+++ Shopping.Infrastructure/Persistence/Repositories/GenericRepository.cs
@@ -0,0 +1,61 @@
+﻿using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+using Shopping.Application.Common.Interfaces;
+using Shopping.Domain.Entities;
+
+namespace Shopping.Infrastructure.Persistence.Repositories
+{
+    public class GenericRepository<T> : IGenericRepository<T>
+        where T : class,IBaseEntity,new()
+    {
+
+
+        protected readonly ApplicationDbContext _applicationDbContext;
+        protected readonly DbSet<T> _dbSet;
+        public GenericRepository(ApplicationDbContext applicationDbContext)
+        {
+            _applicationDbContext = applicationDbContext;
+            _dbSet = _applicationDbContext.Set<T>();
+        }
+        public async Task<bool> Add(T entity)
+        {
+            await _dbSet.AddAsync(entity);
+            var result = await _applicationDbContext.SaveChangesAsync();
+            return result>0;
+        }
+
+        public async Task<bool> Delete(T entity)
+        {
+            var current = await _dbSet.FirstOrDefaultAsync(x => x.Id == entity.Id);
+            if (current == null) throw new Exception("Entity not found!");
+
+            _dbSet.Remove(current);
+            var result = await _applicationDbContext.SaveChangesAsync();
+            return result>0;
+        }
+
+        public async Task<bool> Update(T entity)
+        {
+            _dbSet.Update(entity);
+            var result = await _applicationDbContext.SaveChangesAsync();
+            return result>0;
+        }
+        
+        public async Task<IEnumerable<T>> Get(Expression<Func<T, bool>> expression)
+        {
+            return await _dbSet.Where(expression).ToListAsync();
+        }
+
+        public async Task<IEnumerable<T>> GetAll()
+        {
+            return await _dbSet.ToListAsync();
+        }
+
+        public async Task<T> GetById(int id)
+        {
+            return await _dbSet.SingleOrDefaultAsync(x => x.Id == id);
+        }
+
+   
+    }
+}
