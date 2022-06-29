482c11801ebc23f6344d761629bb2634e8c85f75
MelihOzaslan01 <MelihOzaslan01@gmail.com>
2022-06-30 00:44:48 +0300

    Database context added

---
+++ Shopping.Application/Common/Interfaces/IApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shopping.Domain.Entities;
+
+namespace Shopping.Application.Common.Interfaces
+{
+    public interface IApplicationDbContext
+    {
+        DbSet<User> Users { get; }
+        DbSet<Domain.Entities.Product> Products { get; }
+        DbSet<ShoppingList> ShoppingLists { get; }
+
+    }
+}
---
+++ Shopping.Infrastructure/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Shopping.Application.Common.Interfaces;
+using Shopping.Domain.Entities;
+
+namespace Shopping.Infrastructure.Persistence
+{
+    public class ApplicationDbContext : IdentityDbContext,IApplicationDbContext
+    {
+        public ApplicationDbContext()
+        {
+            
+        }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            if (!options.IsConfigured)
+            {
+                options.UseSqlServer("A FALLBACK CONNECTION STRING");
+            }
+        }
+        public DbSet<ShoppingList> ShoppingLists { get; set; }
+        
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
