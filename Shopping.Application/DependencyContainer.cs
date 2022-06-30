5aef694d8fd6f145009cc77f86b6dc6143457b58
MelihOzaslan01 <MelihOzaslan01@gmail.com>
2022-06-30 15:46:45 +0300

    IOC container added

---
+++ Shopping.Application/DependencyContainer.cs
@@ -0,0 +1,32 @@
+﻿using System.Reflection;
+using AutoMapper;
+using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using Shopping.Application.Common.Behaviours;
+using Shopping.Application.Mapping.AutoMapper;
+
+namespace Shopping.Application
+{
+    public static class DependencyContainer
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+            
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviour<,>));
+
+            var mapperConfig = new MapperConfiguration(mc =>
+            {
+                mc.AddProfile(new MappingProfile());
+            });
+
+            IMapper mapper = mapperConfig.CreateMapper();
+            services.AddSingleton(mapper);
+            
+            return services;
+        }
+    }
+}
