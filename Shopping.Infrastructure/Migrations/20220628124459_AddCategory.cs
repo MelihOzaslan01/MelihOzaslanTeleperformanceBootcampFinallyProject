16ee227f2cc91cb825008b91e2daf60c477e75a8
MelihOzaslan01 <MelihOzaslan01@gmail.com>
2022-06-30 16:00:36 +0300

    Migration added

---
+++ Shopping.Infrastructure/Migrations/20220628124459_AddCategory.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Shopping.Infrastructure.Migrations
+{
+    public partial class AddCategory : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CompleteDate",
+                table: "ShoppingLists",
+                type: "datetime2",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Category",
+                table: "ShoppingLists",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Category",
+                table: "ShoppingLists");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CompleteDate",
+                table: "ShoppingLists",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldNullable: true);
+        }
+    }
+}
