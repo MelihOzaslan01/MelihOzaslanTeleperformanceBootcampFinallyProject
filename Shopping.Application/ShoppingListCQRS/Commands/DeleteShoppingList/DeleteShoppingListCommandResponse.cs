0b3037cd2fec551e46d48fb3afc47d1ac9b5a4bb
MelihOzaslan01 <MelihOzaslan01@gmail.com>
2022-06-30 20:38:10 +0300

    Delete shopping list command and handler added

---
+++ Shopping.Application/ShoppingListCQRS/Commands/DeleteShoppingList/DeleteShoppingListCommandResponse.cs
@@ -0,0 +1,6 @@
+namespace Shopping.Application.ShoppingListCQRS.Commands.DeleteShoppingList;
+
+public class DeleteShoppingListCommandResponse
+{
+    public bool IsSuccess { get; set; }
+}
---
+++ Shopping.Application/ShoppingListCQRS/Handlers/CommandHandlers/DeleteShoppingList/DeleteShoppingListCommandHandler.cs
@@ -0,0 +1,44 @@
+using MediatR;
+using Microsoft.Extensions.Caching.Distributed;
+using Shopping.Application.Common.Interfaces.Repositories;
+using Shopping.Application.ShoppingListCQRS.Commands.DeleteShoppingList;
+using Shopping.Application.ShoppingListCQRS.Commands.UpdateShoppingList;
+
+namespace Shopping.Application.ShoppingListCQRS.Handlers.CommandHandlers.DeleteShoppingList;
+
+public class DeleteShoppingListCommandHandler:IRequestHandler<DeleteShoppingListCommandRequest,DeleteShoppingListCommandResponse>
+{
+    private readonly IShoppingListRepository _shoppingListRepository;
+    private readonly IDistributedCache _distributedCache;
+
+    public DeleteShoppingListCommandHandler(IShoppingListRepository shoppingListRepository, IDistributedCache distributedCache)
+    {
+        _shoppingListRepository = shoppingListRepository;
+        _distributedCache = distributedCache;
+    }
+
+    public async Task<DeleteShoppingListCommandResponse> Handle(DeleteShoppingListCommandRequest request, CancellationToken cancellationToken)
+    {
+        var shoppingList = new Domain.Entities.ShoppingList
+        {
+            Id = request.Id,
+            Title = request.Title,
+            Category = request.Category,
+            CompleteDate = null,
+            CreateDate = DateTime.Now,
+            Description = request.Description,
+            IsCompleted = false
+        };
+        var deleteShoppingListCommandResponse = new DeleteShoppingListCommandResponse();
+        var result = await _shoppingListRepository.Delete(shoppingList);
+
+        deleteShoppingListCommandResponse.IsSuccess = result;
+
+        if (deleteShoppingListCommandResponse.IsSuccess)
+        {
+            await _distributedCache.RemoveAsync("shoppingLists", cancellationToken);
+        }
+
+        return deleteShoppingListCommandResponse;
+    }
+}
