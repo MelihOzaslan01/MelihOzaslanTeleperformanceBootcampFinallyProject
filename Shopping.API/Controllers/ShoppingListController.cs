c57fd0977bfdac5f3e414931bf810191f0ee0457
MelihOzaslan01 <MelihOzaslan01@gmail.com>
2022-07-02 02:05:02 +0300

    Updated shopping list controller

--- Shopping.API/Controllers/ShoppingListController.cs
+++ Shopping.API/Controllers/ShoppingListController.cs
@@ -1,9 +1,12 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Shopping.API.Controllers.Interfaces;
+using Shopping.Application.ShoppingListCQRS.Commands.CompleteShoppingList;
 using Shopping.Application.ShoppingListCQRS.Commands.CreateShoppingList;
 using Shopping.Application.ShoppingListCQRS.Commands.DeleteShoppingList;
 using Shopping.Application.ShoppingListCQRS.Commands.UpdateShoppingList;
+using Shopping.Application.ShoppingListCQRS.Queries.AdminGetShoppingLists;
 using Shopping.Application.ShoppingListCQRS.Queries.GetShoppingListByCategory;
 using Shopping.Application.ShoppingListCQRS.Queries.GetShoppingListByCompleted;
 using Shopping.Application.ShoppingListCQRS.Queries.GetShoppingListByCompleteDate;
@@ -15,7 +18,7 @@ using Shopping.Application.ShoppingListCQRS.Queries.GetShoppingLists;
 
 namespace Shopping.API.Controllers;
 
-[Authorize]
+[Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme,Roles = "User")]
 [Route("api/[controller]")]
 [ApiController]
 public class ShoppingListController:BaseController
@@ -53,10 +56,24 @@ public class ShoppingListController:BaseController
 
         return Ok(result);
     }
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme,Roles = "Admin")]
     [HttpGet("GetShoppingLists")]
     public async Task<IActionResult> GetShoppingLists()
     {
         var command = new GetShoppingListsQueryRequest();
+        var result = await Mediator.Send(command);
+        if (!result.IsSuccess)
+        {
+            return BadRequest(result);
+        }
+
+        return Ok(result);
+    }
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme,Roles = "Admin")]
+    [HttpGet("AdminGetShoppingLists")]
+    public async Task<IActionResult> AdminGetShoppingLists()
+    {
+        var command = new AdminGetShoppingListsQueryRequest();
         var result = await Mediator.Send(command);
         if (!result.IsSuccess)
         {
@@ -141,6 +158,17 @@ public class ShoppingListController:BaseController
     public async Task<IActionResult> GetShoppingListByCompleteDate(DateTime date)
     {
         var command = new GetShoppingListByCompleteDateQueryRequest(){CompleteDate = date};
+        var result = await Mediator.Send(command);
+        if (!result.IsSuccess)
+        {
+            return BadRequest(result);
+        }
+
+        return Ok(result);
+    }
+    [HttpPost("CompleteShoppingList")]
+    public async Task<IActionResult> CompleteShoppingList(CompleteShoppingListCommandRequest command)
+    {
         var result = await Mediator.Send(command);
         if (!result.IsSuccess)
         {
